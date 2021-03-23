9a250fe02cc374c8e82787c40afb5c26c5c46c72
meaghan jennings <meaghanljennings@gmail.com>
2021-03-23 22:08:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
     @items = []
   end  
    
-  def add_item(title, price, quantity = 0)
+  def add_item(title, price, quantity = 1)
     quantity.times do 
       @items << title 
     end
